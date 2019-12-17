9f35e3545cb9f95d2a8a4a3a25885f4f2481cf32
Ariello05 <heriu@wp.pl>
2019-12-17 02:06:28 +0100

    Working labeling + style changes

--- app/src/main/java/com/example/streetlights/MainActivity.kt
+++ app/src/main/java/com/example/streetlights/MainActivity.kt
@@ -1,52 +1,35 @@
 package com.example.streetlights
 
+
+import android.os.Bundle
+import android.os.Environment
+import android.view.SurfaceView
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-import android.os.Environment;
-import android.os.Bundle;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Toast;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.*;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.dnn.Net;
-import org.opencv.imgproc.Imgproc;
-
-import org.opencv.dnn.Dnn;
-import org.opencv.utils.Converters;
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.OpenCVLoader
+import org.opencv.core.*
+import org.opencv.dnn.Dnn
+import org.opencv.dnn.Net
+import org.opencv.imgproc.Imgproc
+import org.opencv.utils.Converters
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewListener2 {
 
-    lateinit var cameraBridgeViewBase: CameraBridgeViewBase
-    lateinit var baseLoaderCallback: BaseLoaderCallback
+    private lateinit var cameraBridgeViewBase: CameraBridgeViewBase
+    private lateinit var baseLoaderCallback: BaseLoaderCallback
 
-    var startYolo = false
-    var firstTimeYolo = false
-    lateinit var tinyYolo: Net
+    private var startYolo = false
+    private var firstTimeYolo = false
+    private lateinit var tinyYolo: Net
 
-    var objectFound = "null"
+    private var objectFound = "null"
 
-    fun YOLO(view: View) {
+    fun start_yolo(view: View) {
 
         val text = objectFound
         val duration = Toast.LENGTH_SHORT
@@ -55,9 +38,9 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
 
         objectFound = "null"
 
-        if (startYolo == false) {
+        if (!startYolo) {
             startYolo = true
-            if (firstTimeYolo == false) {
+            if (!firstTimeYolo) {
                 firstTimeYolo = true
                 val tinyYoloCfg: String =
                     Environment.getExternalStorageDirectory().toString() + "/dnns/yolov3-tiny.cfg"
@@ -75,7 +58,7 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
         setContentView(R.layout.activity_main)
 
         cameraBridgeViewBase = findViewById(R.id.CameraView)
-        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE)
+        cameraBridgeViewBase.visibility = SurfaceView.VISIBLE
         cameraBridgeViewBase.setCvCameraViewListener(this)
 
         baseLoaderCallback = object : BaseLoaderCallback(this) {
@@ -83,7 +66,7 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
                 super.onManagerConnected(status)
 
                 when (status) {
-                    BaseLoaderCallback.SUCCESS -> cameraBridgeViewBase.enableView()
+                    SUCCESS -> cameraBridgeViewBase.enableView()
                     else -> super.onManagerConnected(status)
                 }
             }
@@ -95,16 +78,16 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
 
         val frame = inputFrame.rgba()
 
-        if (startYolo == true) {
+        if (startYolo) {
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB)
             val imageBlob = Dnn.blobFromImage(
                 frame, 0.00392, Size(416.0,416.0), Scalar(0.0, 0.0, 0.0),false, false
             )
 
-            tinyYolo!!.setInput(imageBlob)
+            tinyYolo.setInput(imageBlob)
 
-            var result: MutableList<Mat> = ArrayList(2)
-            var outBlobNames: MutableList<String> = ArrayList()
+            val result: MutableList<Mat> = ArrayList(2)
+            val outBlobNames: MutableList<String> = ArrayList()
 
             outBlobNames.add(0,"yolo_23")
             outBlobNames.add(1, "yolo_23")
@@ -139,19 +122,20 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
                 }
             }
 
-            val ArrayLength = confs.size
-            if (ArrayLength >= 1) { // Apply non-maximum suppression procedure.
+            val arrayLength = confs.size
+            if (arrayLength >= 1) { // Apply non-maximum suppression procedure.
 
                 objectFound = "person"
 
                 val nmsThresh = 0.2f
                 val confidences = MatOfFloat(Converters.vector_float_to_Mat(confs))
 
-                val boxesArray: Array<Rect> = arrayOf(rects[0])
+                //val boxesArray: Array<Rect> = arrayOf(rects[0])
+                val boxesArray: Array<Rect> = arrayOf(*rects.toTypedArray())
 
                 val boxes = MatOfRect(*boxesArray)
                 val indices = MatOfInt()
-                /*
+
 
                 Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices)
                 // Draw result boxes:
@@ -161,88 +145,6 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
                     val box: Rect = boxesArray[idx]
                     val idGuy = clsIds[idx]
                     val conf = confs[idx]
-                    val cocoNames: MutableList<String> = arrayListOf(
-                        "a person",
-                        "a bicycle",
-                        "a motorbike",
-                        "an airplane",
-                        "a bus",
-                        "a train",
-                        "a truck",
-                        "a boat",
-                        "a traffic light",
-                        "a fire hydrant",
-                        "a stop sign",
-                        "a parking meter",
-                        "a car",
-                        "a bench",
-                        "a bird",
-                        "a cat",
-                        "a dog",
-                        "a horse",
-                        "a sheep",
-                        "a cow",
-                        "an elephant",
-                        "a bear",
-                        "a zebra",
-                        "a giraffe",
-                        "a backpack",
-                        "an umbrella",
-                        "a handbag",
-                        "a tie",
-                        "a suitcase",
-                        "a frisbee",
-                        "skis",
-                        "a snowboard",
-                        "a sports ball",
-                        "a kite",
-                        "a baseball bat",
-                        "a baseball glove",
-                        "a skateboard",
-                        "a surfboard",
-                        "a tennis racket",
-                        "a bottle",
-                        "a wine glass",
-                        "a cup",
-                        "a fork",
-                        "a knife",
-                        "a spoon",
-                        "a bowl",
-                        "a banana",
-                        "an apple",
-                        "a sandwich",
-                        "an orange",
-                        "broccoli",
-                        "a carrot",
-                        "a hot dog",
-                        "a pizza",
-                        "a doughnut",
-                        "a cake",
-                        "a chair",
-                        "a sofa",
-                        "a potted plant",
-                        "a bed",
-                        "a dining table",
-                        "a toilet",
-                        "a TV monitor",
-                        "a laptop",
-                        "a computer mouse",
-                        "a remote control",
-                        "a keyboard",
-                        "a cell phone",
-                        "a microwave",
-                        "an oven",
-                        "a toaster",
-                        "a sink",
-                        "a refrigerator",
-                        "a book",
-                        "a clock",
-                        "a vase",
-                        "a pair of scissors",
-                        "a teddy bear",
-                        "a hair drier",
-                        "a toothbrush"
-                    )
 
                     val intConf = (conf * 100).toInt()
                     Imgproc.putText(
@@ -257,7 +159,7 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
                     Imgproc.rectangle(frame, box.tl(), box.br(), Scalar(255.0, 0.0, 0.0), 2)
                 }
 
-                 */
+
             }
         }
         return frame
@@ -265,7 +167,7 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
 
     override fun onCameraViewStarted(width: Int, height: Int) {
 
-        if (startYolo == true) {
+        if (startYolo) {
             val tinyYoloCfg =
                 Environment.getExternalStorageDirectory().toString() + "/dnns/yolov3-tiny.cfg"
             val tinyYoloWeights =
@@ -294,16 +196,22 @@ class MainActivity : AppCompatActivity() , CameraBridgeViewBase.CvCameraViewList
     override fun onPause() {
         super.onPause()
 
-        if(cameraBridgeViewBase!=null){
-            cameraBridgeViewBase.disableView()
-        }
+        cameraBridgeViewBase.disableView()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        if (cameraBridgeViewBase!=null){
-            cameraBridgeViewBase.disableView()
-        }
+        cameraBridgeViewBase.disableView()
     }
+
+    private val cocoNames: MutableList<String> = arrayListOf(
+        "green_light",
+        "green_right",
+        "green_left",
+        "red_light",
+        "red_right",
+        "red_left",
+        "yellow_light"
+    )
 }
